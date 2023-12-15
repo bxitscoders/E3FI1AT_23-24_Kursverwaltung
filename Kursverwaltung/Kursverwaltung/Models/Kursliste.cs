724e7c341073364830f6c1f25190d8029bb9e015
itsEchD <98314899+itsEchD@users.noreply.github.com>
2023-12-15 09:42:01 +0100

    KurslisteView, KurslisteVM, Klassen Kurs und Kursliste mit Platzhaltern

---
+++ Kursverwaltung/Kursverwaltung/Models/Kursliste.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace Kursverwaltung.Models
+{
+    public class Kursliste
+    {
+        public ObservableCollection<Kurs> Kurse { get; set; }
+
+        public Kursliste()
+        {
+            Kurse = new ObservableCollection<Kurs>();
+
+            Kurse.Add(new Kurs
+            {
+                //Platzhalter -> Ersetzen durch Abruf aus DB
+                Name = "Beispielkurs 1",
+                Datum = new DateTime(2023, 12, 31),
+                Anmeldefrist = new DateTime(2023, 12, 28),
+                AktuelleTeilnehmerzahl = 5,
+                MaximaleTeilnehmerzahl = 15
+            });
+
+            Kurse.Add(new Kurs
+            {
+                //Platzhalter -> Ersetzen durch Abruf aus DB
+
+                Name = "Beispielkurs 2",
+                Datum = new DateTime(2024, 1, 15),
+                Anmeldefrist = new DateTime(2024, 1, 10),
+                AktuelleTeilnehmerzahl = 10,
+                MaximaleTeilnehmerzahl = 20
+            });
+
+
+        }
+    }
+}
---
+++ Kursverwaltung/Kursverwaltung/ViewModels/KurslisteVM.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+
+namespace Kursverwaltung.ViewModels
+{
+    public class KurslisteVM : INotifyPropertyChanged
+    {
+        private Models.Kursliste kursliste;
+
+        public Models.Kursliste Kursliste
+        {
+            get { return kursliste; }
+            set
+            {
+                kursliste = value;
+                OnPropertyChanged(nameof(Kursliste));
+            }
+        }
+
+        public KurslisteVM()
+        {
+            Kursliste = new Models.Kursliste();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}

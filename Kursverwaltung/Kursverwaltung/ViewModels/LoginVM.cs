5e74018c9c0c371d56b45a6156a6bbcd8638ca12
ariba0 <105052973+ariba0@users.noreply.github.com>
2023-12-18 11:12:17 +0100

    Login Revamp + setup login viewmodel

---
+++ Kursverwaltung/Kursverwaltung/ViewModels/LoginVM.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.ComponentModel;
+using System.Security;
+using System.Windows;
+using System.Windows.Input;
+using Kursverwaltung.Models.Utilities;
+
+namespace Kursverwaltung.ViewModels
+{
+    public class LoginVM : INotifyPropertyChanged
+    {
+        private string username;
+        private string password;
+
+        public SecureString SecurePassword { private get; set; }
+
+        public string Username
+        {
+            get { return username;  }
+            set
+            {
+                username = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+
+        public ICommand LoginCommand { get; private set; }
+
+        public LoginVM()
+        {
+            LoginCommand = new RelayCommand(UserLogin);
+        }
+
+        private void UserLogin(object parameter)
+        {
+            if (isAdmin(Username, Password))
+            {
+                MessageBox.Show("200: User is Admin.");
+            } else
+            {
+                MessageBox.Show("200: User is User");
+            }
+        }
+
+        private bool isAdmin(string username, string password)
+        {
+            return (username == "admin" && password == "admin"); 
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Kursverwaltung/Kursverwaltung/Views/Login.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Kursverwaltung.Views
+{
+    /// <summary>
+    /// Interaktionslogik für Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+            DataContext = new ViewModels.AdminAnmeldungVM();
+        }
+
+        private void PWBoxChanged(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is ViewModels.AdminAnmeldungVM viewModel)
+            {
+                if (sender is PasswordBox passwordBox)
+                {
+                    viewModel.SecurePassword = passwordBox.SecurePassword;
+                }
+            }
+        }
+    }
+}
